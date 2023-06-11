33f46f0165b7ebac6b1410493ce8c4cad7aeb750
Romamill <122994080+Romamill@users.noreply.github.com>
2023-06-11 09:32:36 +0300

    task02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
 
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const iframe = document.querySelector('iframe');
+  const player = new VimeoPlayer(iframe);
+  const localStorageKey = 'videoplayer-current-time';
+  let timeUpdateListener;
+
+  const handleTimeUpdate = throttle(data => {
+    const currentTime = data.seconds;
+    localStorage.setItem(localStorageKey, currentTime.toString());
+  }, 1000);
+
+  const restorePlayback = () => {
+    const savedTime = localStorage.getItem(localStorageKey);
+    if (savedTime !== null) {
+      const currentTime = parseFloat(savedTime);
+      player.setCurrentTime(currentTime)
+        .then(() => {
+          player.off('timeupdate', timeUpdateListener);
+          timeUpdateListener = player.on('timeupdate', handleTimeUpdate);
+        })
+        .catch(error => {
+          console.error('Failed to set current time:', error);
+        });
+    } else {
+      timeUpdateListener = player.on('timeupdate', handleTimeUpdate);
+    }
+  };
+
+  restorePlayback();
+});
