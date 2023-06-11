89cf8f91d30c3775a8b7ea21ec9fa5dcf88e4644
Romamill <122994080+Romamill@users.noreply.github.com>
2023-06-11 13:00:41 +0300

    task03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
 
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageInput = feedbackForm.querySelector('textarea[name="message"]');
+
+const savedState = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+
+emailInput.value = savedState.email || '';
+messageInput.value = savedState.message || '';
+
+const saveFormState = throttle(() => {
+    let state = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(state));
+}, 500);
+
+emailInput.addEventListener('input', saveFormState);
+messageInput.addEventListener('input', saveFormState);
+
+feedbackForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    let state = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+    console.log('Form submitted:', state);
+
+    emailInput.removeEventListener('input', saveFormState);
+    messageInput.removeEventListener('input', saveFormState);
+
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageInput.value = '';
+});
